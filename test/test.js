aa4b8e83a6de3f27428409e701bcad1e695f961c
jbong <jbong.net@gmail.com>
2014-04-17 13:59:23 +0100

    db

--- test/test.js
+++ test/test.js
@@ -6,6 +6,19 @@ var mp = require('../lib');
 var assert = require('assert');
 
 
+before(function(done) {
+    mp.MongoClient.connect("mongodb://localhost:27017/mptestdb")
+        .then(function(db){
+            return db.dropDatabase();
+        })
+        .then(function() {
+            done();
+        })
+        .fail(function(err) {
+            done(err);
+        })
+});
+
 describe('MongoClient', function(){
 
     it('should connect to mongodb via mongodb:// uri', function(done) {
@@ -17,11 +30,10 @@ describe('MongoClient', function(){
     });
 });
 
-
 describe('Db', function(){
 
     it('should open', function(done) {
-        var db = new mp.Db('test', new mp.Server('localhost', 27017), {w:1});
+        var db = new mp.Db('mptestdb', new mp.Server('localhost', 27017), {w:1});
         db.open().then(function(db) {
             done();
         });
@@ -29,7 +41,7 @@ describe('Db', function(){
     });
 
     it('should fetch a collection', function(done) {
-        mp.MongoClient.connect("mongodb://localhost:27017/test")
+        mp.MongoClient.connect("mongodb://localhost:27017/mptestdb")
             .then(function(db){
                 assert.notEqual(db, null, 'db null');
                 return db.collection('test');
@@ -45,7 +57,7 @@ describe('Db', function(){
     });
 
     it('should fetch collection names', function(done) {
-        mp.MongoClient.connect("mongodb://localhost:27017/test")
+        mp.MongoClient.connect("mongodb://localhost:27017/mptestdb")
             .then(function(db){
                 assert.notEqual(db, null, 'db null');
                 return db.collectionNames();
@@ -58,5 +70,67 @@ describe('Db', function(){
                 done(err);
             })
 
+    });
+
+    it('should create a collection', function(done) {
+        mp.MongoClient.connect("mongodb://localhost:27017/mptestdb")
+            .then(function(db){
+                assert.notEqual(db, null, 'db null');
+                return db.createCollection('testcol');
+            })
+            .then(function(col) {
+                assert.notEqual(col, null, 'col null');
+                done();
+            })
+            .fail(function(err) {
+                done(err);
+            })
+
+    });
+
+    it('should drop itself', function(done) {
+        mp.MongoClient.connect("mongodb://localhost:27017/mptestdb")
+            .then(function(db){
+                assert.notEqual(db, null, 'db null');
+                return db.dropDatabase();
+            })
+            .then(function(result) {
+                done();
+            })
+            .fail(function(err) {
+                done(err);
+            })
+    });
+
+    it('should get all db stats', function(done) {
+        mp.MongoClient.connect("mongodb://localhost:27017/mptestdb")
+            .then(function(db){
+                assert.notEqual(db, null, 'db null');
+                return db.stats();
+            })
+            .then(function(stats) {
+                assert.equal(stats.db, 'mptestdb', 'assert db name');
+                done();
+            })
+            .fail(function(err) {
+                done(err);
+            })
+    });
+
+    it('should rename a collection', function(done) {
+        mp.MongoClient.connect("mongodb://localhost:27017/mptestdb")
+            .then(function(db) {
+                assert.notEqual(db, null, 'db null');
+                return db.createCollection('testcol')
+                    .then(function (col1) {
+                        return db.renameCollection('testcol', 'testcol2');
+                    })
+                    .then(function (col2) {
+                        done();
+                    })
+            })
+            .fail(function(err) {
+                done(err);
+            })
     });
 });
